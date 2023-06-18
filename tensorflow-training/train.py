5e504e97f0d71109063c7082153b537a0bfbe3d9
Wemzi <55751876+Wemzi@users.noreply.github.com>
2023-06-18 20:54:56 +0200

    parsing image data into numpy arrays, adding example from training set, dataset info added

--- tensorflow-training/train.py
+++ tensorflow-training/train.py
@@ -1,24 +1,39 @@
+# import tester after importing tensorflow, to make sure correct tf version is imported
+# see examples for training dataset in dataset_examples
+# origin of dataset: https://www.v7labs.com/open-datasets/face-head-segmentation-dataset
+# import picture size: 256x256 (x 3 values because of RGB)
 import xml.etree.ElementTree as ET
 import tensorflow as tf
 import os
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Input, Conv2D, Dropout, MaxPooling2D, AveragePooling2D, Flatten, Dense, Activation
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow.keras.optimizers
-# import tester after importing tensorflow, to make sure correct tf version is imported
-#import picture size: 256 x 256 (x 3 values because of RGB)
+
 tree = ET.parse('../headsegmentation_dataset_ccncsa/training.xml')
 root = tree.getroot()
 srcimages = root.findall("srcimg")
 lblimages = root.findall("labelimg")
-labelboxes = root.findall("bboxes")
-
+labelboxes = root.findall("bboxes") #wont be used for now
+srcimage_list = list()
+lblimage_list = list()
 for index in range(16056): #seems like there are less boxes than images
-    srcimage = srcimages[index]
+    srcimage_path = srcimages[index].attrib['name'].replace("\\","/")
+    lblimage_path = lblimages[index].attrib['name'].replace("\\","/")
+    srcimage_list.append(plt.imread(f"headsegmentation_dataset_ccncsa/{srcimage_path}"))
+    lblimage_list.append(plt.imread(f"headsegmentation_dataset_ccncsa/{lblimage_path}"))
+    print("tick")
     #TODO: handle file loadings into numpy arrays for images
-    lblimage = lblimages[index]
-    labelbox = labelboxes[index]
+#filter unscaled images, hopefully index stays right
+srcimage_list = [elem for elem in srcimage_list if elem.shape == (256,256,3)]
+lblimage_list = [elem for elem in lblimage_list if elem.shape == (256,256,3)]
+
+srcimages_np = np.asarray(srcimage_list, dtype=np.float32)
+lblimages_np = np.asarray(lblimage_list, dtype=np.float32)
+
+
 print(root.tag)
